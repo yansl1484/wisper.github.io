16c7da32d34455fb464ab4bb3d97647fc9c8bbdd
yansl <yansl1484@163.com>
2019-05-02 11:56:26 +0800

    backup at 20190502115624

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 git checkout hexo
 echo "-------------backup--------------"
-time=$(date "+%Y-%m-%d %H:%M:%S")
-git add . && git commit -m "backup at ${time}" && git push origin hexo
+time="`date +%Y%m%d%H%M%S`"
+git add . && git commit -m "backup at $time" && git push origin hexo
 echo "--------------backup success--------------"
